3eece86471f9a0f547596e3bcfb7fb28ef99db45
xifeng_alven <xifeng_alven@foxmail.com>
2024-09-07 14:37:49 +0800

    用栈实现先进先出队列 - 简单

---
+++ src/main/java/stackandqueue/Solution232.java
@@ -0,0 +1,60 @@
+package stackandqueue;
+
+import java.util.Stack;
+
+public class Solution232 {
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop()); // return 1, queue is [2]
+        System.out.println(myQueue.empty()); // return false
+    }
+
+
+
+}
+
+
+class MyQueue {
+
+    // 负责入
+    private Stack<Integer> stack1;
+    // 负责出
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty() && !stack1.isEmpty()){
+            int size = stack1.size();
+            for (int i = 0; i < size; i++) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty() && !stack1.isEmpty()){
+            int size = stack1.size();
+            for (int i = 0; i < size; i++) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack2.isEmpty() && stack1.isEmpty();
+    }
+}

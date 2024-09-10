a30d4ec68cc6d1946caef005ce09446f5dfd4ed8
xifeng_alven <xifeng_alven@foxmail.com>
2024-09-10 12:35:58 +0800

    前k个高频元素——中等，暴力解法做出来了。可以使用堆的解法，下午再改一下

---
+++ src/main/java/stackandqueue/Solution347.java
@@ -0,0 +1,43 @@
+package stackandqueue;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Solution347 {
+
+
+    public static void main(String[] args) {
+        int[] ints = new Solution347().topKFrequent(new int[]{1,1,1,2,2,3}, 2);
+        for (int anInt : ints) {
+            System.out.print(anInt);
+        }
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] res = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            int max = Integer.MIN_VALUE;
+            int maxKey = 0;
+            for (Integer integer : map.keySet()) {
+                Integer i1 = map.get(integer);
+                if (i1 > max) {
+                    max = i1;
+                    maxKey = integer;
+                }
+            }
+            map.remove(maxKey);
+            res[i] = maxKey;
+        }
+        return res;
+    }
+}
